6e437f7ebae591d5cc8d3ffcece9692cee76a07e
SysHaxUBs <126807899+SysHaxUBs@users.noreply.github.com>
2023-06-16 10:00:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    console.log("SHXLOGS - homepage visit.")
+    res.send('This is the shadow client for project rebirth, an unbl*cked games site for your enjoyment. This is an early test version of the site.')
 })
 app.listen(process.env.PORT || 3000)
