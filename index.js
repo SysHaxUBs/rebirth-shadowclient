58346e7efc3571c357ce409d9a1cebe1de78db9c
SysHaxUBs <126807899+SysHaxUBs@users.noreply.github.com>
2023-06-16 13:22:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+var nodemailer = require('nodemailer');
 const express = require('express')
 const app = express()
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+
 app.all('/', (req, res) => {
     console.log("SHXLOGS - homepage visit.")
     res.send('This is the shadow client for project rebirth, an unbl*cked games site for your enjoyment. This is an early test version of the site.')
