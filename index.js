99d77fdf32831441ba15a8af0ff2ba71c889a518
SysHaxUBs <126807899+SysHaxUBs@users.noreply.github.com>
2023-06-16 13:28:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-var nodemailer = require('nodemailer');
 const express = require('express')
 const app = express()
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
-  }
-});
-
-var mailOptions = {
-  from: 'youremail@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-
 
 app.all('/', (req, res) => {
     console.log("SHXLOGS - homepage visit.")
